836a60c1dd69fb5d900ae26c4238068c900fe46c
Phil <philgee@bu.edu>
2022-02-24 21:32:05 -0500

    Included database/user creation with initialize script

--- server/initializeDB.js
+++ server/initializeDB.js
@@ -1,9 +1,38 @@
+// ============================================
+// Create Database and readWrite user
+
+import mongodb from 'mongodb'
+const url = "mongodb://localhost:27017"
+
+mongodb.MongoClient.connect(url, async (err, client) => {
+    if (err) console.log(err)
+    const db = client.db('vintkoshop')
+    await db.admin().removeUser('vintko')
+    await db.admin().addUser('vintko', 'vintko_secret', {
+        roles: [{
+            role: 'readWrite',
+            db: 'vintkoshop'
+        }]
+    })
+    client.close()
+})
+
+// ============================================
+// 
+
+import messageDB from './model/messageDB.js'
+const Message = messageDB.getModel()
 import userDB from './model/userDB.js'
 const User = userDB.getModel()
+import watchDB from './model/watchDB.js'
+const Watch = watchDB.getModel()
+
+Message.collection.drop()
+User.collection.drop()
+Watch.collection.drop()
 
 // Initialize default user accounts (admin and user)
-const userDefaultInitialize = async (req, res) => {
-    await User.deleteMany({})
+const defaultInitialize = async (req, res) => {
     const userAdmin = new User({
         email: 'vintko_admin@vintko.com',
         password: '123123',
@@ -23,4 +52,4 @@ const userDefaultInitialize = async (req, res) => {
     process.exit()
 }
 
-userDefaultInitialize()
+defaultInitialize()
