35a98a786b9bab6ffaf3ebd85cbeb2d9cf5f5574
Phil Gee <philgee@bu.edu>
2022-02-26 17:15:29 -0500

    Created instructions and removed faulty initialize process

--- server/initializeDB.js
+++ server/initializeDB.js
@@ -1,35 +1,5 @@
-// ============================================
-// Create Database and readWrite user
-
-import mongodb from 'mongodb'
-const url = "mongodb://localhost:27017"
-
-mongodb.MongoClient.connect(url, async (err, client) => {
-    if (err) console.log(err)
-    const db = client.db('vintkoshop')
-    await db.admin().removeUser('vintko')
-    await db.admin().addUser('vintko', 'vintko_secret', {
-        roles: [{
-            role: 'readWrite',
-            db: 'vintkoshop'
-        }]
-    })
-    client.close()
-})
-
-// ============================================
-// 
-
-import messageDB from './model/messageDB.js'
-const Message = messageDB.getModel()
 import userDB from './model/userDB.js'
 const User = userDB.getModel()
-import watchDB from './model/watchDB.js'
-const Watch = watchDB.getModel()
-
-Message.collection.drop()
-User.collection.drop()
-Watch.collection.drop()
 
 // Initialize default user accounts (admin and user)
 const defaultInitialize = async (req, res) => {
