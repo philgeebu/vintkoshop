3e1e75f5b38c19b5b7a46434b8f45eab1da381c8
Phil Gee <philgee@bu.edu>
2022-02-26 23:31:18 -0500

    tweaks to initialize

--- server/initializeDB.js
+++ server/initializeDB.js
@@ -3,6 +3,13 @@ const User = userDB.getModel()
 
 // Initialize default user accounts (admin and user)
 const defaultInitialize = async (req, res) => {
+    try {
+        await User.collection.drop()
+        console.log('Current "User" collection dropped. Moving on...')
+    } catch (err) {
+        if (err.code === 26) console.log('No "User" collection to drop. Moving on...')
+        else console.log(err)
+    }
     const userAdmin = new User({
         email: 'vintko_admin@vintko.com',
         password: '123123',
@@ -18,6 +25,7 @@ const defaultInitialize = async (req, res) => {
         userAdmin.save(),
         userRegular.save()
     ])
+    console.log('"User" collection and default users (re)created:')
     console.log(await User.find({}))
     process.exit()
 }
